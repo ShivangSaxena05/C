0be29f3b367a68eace4f811fe5e31a54d409fed7
Shivang Saxena <82094331+ShivangSaxena05@users.noreply.github.com>
2023-12-27 19:45:51 +0530

    Update cfile.c

--- cfile.c
+++ cfile.c
@@ -1,35 +1,34 @@
 #include<stdio.h>
 #include <stdlib.h>
-void insertion(struct node *a){
-  struct node *a;
-  a=malloc(sizeof(struct node));
-  a->data=50;
-  a->next=first;
-  head=a;
-}
 struct node{
   int data;
   struct node *next;
 };
+void insertion(struct node *a){
+  struct node *a;
+  a=malloc(sizeof(struct node));
+  a->data=50;
+  a->next=head;
+  head=a;
+}
 int main(){
-  struct node *head;
   struct node *first=NULL;
   struct node *second=NULL;
   struct node *third=NULL;
   struct node *fourth=NULL;
-  first=malloc(sizeof(struct node));
+  head=malloc(sizeof(struct node));
   second=malloc(sizeof(struct node));
   third=malloc(sizeof(struct node));
   fourth=malloc(sizeof(struct node));
-  first->data=10;
-  first->next=second;
+  head->data=10;
+  head->next=second;
   second->data=20;
   second->next=third;
   third->data=30;
   third->next=fourth;
   fourth->data=40;
   fourth->next=NULL;
-  insertion(a);
+  head=insertion(head)
   while(head!=NULL){
     printf("%d \n",head->data);
     head=head->next;
